1df4c35e8e97c305aecfcf45d07cab26100a1520
Max Boone <max.boone@maxem.energy>
2024-11-05 00:21:11 +0100

    feat: initial commit

---
+++ site/src/lib/components/layout/MenuBar.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import User from "lucide-svelte/icons/circle-user-round";
+  import CirclePlus from "lucide-svelte/icons/circle-plus";
+  import Search from "lucide-svelte/icons/search";
+
+  import LightSwitch from "./LightSwitch.svelte";
+  import DocumentDropdown from "./DocumentDropdown.svelte";
+  import { Button } from "$lib/components/ui/button";
+</script>
+
+<header class="w-full p-2 gap-1 border-b flex flex-row justify-between">
+  <DocumentDropdown />
+  <nav>
+    <Button variant="outline" size="icon">
+      <Search class="w-5" />
+    </Button>
+    <Button variant="outline" size="icon">
+      <CirclePlus class="w-5" />
+    </Button>
+    <Button variant="outline" size="icon">
+      <User class="w-5" />
+    </Button>
+    <LightSwitch />
+  </nav>
+</header>
---
+++ site/src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import MenuBar from "$lib/components/layout/MenuBar.svelte";
+  import { ModeWatcher } from "mode-watcher";
+  import "../app.css";
+</script>
+
+<!-- Contexts -->
+<ModeWatcher />
+
+<MenuBar />
+<slot />
